20494fec04e40aeef4279c43726558e6c3df725a
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2024-05-16 14:24:14 +0100

    hero

--- src/components/Imagehero.tsx
+++ src/components/Imagehero.tsx
@@ -4,9 +4,8 @@ import React from "react";
 import { ImagesSlider } from "./ui/images-slider";
 import { FaArrowDownLong } from "react-icons/fa6";
 
-
 export function Imagehero() {
-  const images = ["/avenger1.jpg", "/avenger1.jpg",];
+  const images = ["/avenger1.jpg", "/avenger1.jpg"];
   return (
     <ImagesSlider className="h-[40rem]" images={images}>
       <motion.div
@@ -23,14 +22,18 @@ export function Imagehero() {
         }}
         className="z-50 flex flex-col justify-center items-center"
       >
-        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+        <motion.p className="font-bold text-4xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
           The hero section slideshow <br /> nobody asked for
         </motion.p>
-        <button className="px-4 py-4 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+        <motion.p className="font-bold px-5 md:max-w-xl text-lg text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+          voluptates qui quidem eos error. Ratione culpa blanditiis aperiam
+          eligendi necessitatibus quasi exercitationem.
+        </motion.p>
+        <button className="px-4 py-4 backdrop-blur-sm border bg-yellow-500/10 border-yellow-500/20 text-white mx-auto text-center rounded-full relative mt-4">
           <span>
             <FaArrowDownLong />
           </span>
-          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
         </button>
       </motion.div>
     </ImagesSlider>
